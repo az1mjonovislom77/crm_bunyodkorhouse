a9d0f832f562371e46f64f7e7643c16ed5e06ed9
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-04-01 11:56:58 +0500

    done

--- projects/api/serializers/showroom_serializers.py
+++ projects/api/serializers/showroom_serializers.py
@@ -1,4 +1,5 @@
 from common.base.serializers_base import BaseReadSerializer
+from projects.models.project_models import Blocks, Projects
 from projects.models.showroom_models import SVG, Showroom
 from rest_framework import serializers
 
@@ -14,11 +15,19 @@ class ShowroomSerializer(serializers.ModelSerializer):
     available_homes = serializers.IntegerField(read_only=True)
     sold_homes = serializers.IntegerField(read_only=True)
     reserved_homes = serializers.IntegerField(read_only=True)
+    project_title = serializers.SerializerMethodField()
+    project_id = serializers.SerializerMethodField()
 
     class Meta:
         model = Showroom
         fields = ['id', 'label', 'blocks_number', 'path', 'navigate_to', 'hover_color', 'default_color', 'homes_count',
-                  'available_homes', 'sold_homes', 'reserved_homes']
+                  'available_homes', 'sold_homes', 'reserved_homes', 'project_title', 'project_id']
 
     def get_label(self, obj):
         return obj.blocks.title if obj.blocks else None
+
+    def get_project_title(self, obj):
+        return obj.blocks.projects.title if obj.blocks.projects else None
+
+    def get_project_id(self, obj):
+        return obj.blocks.projects.id if obj.blocks.projects else None
